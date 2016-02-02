c25ebf31a1da22f7632d1542248affea6e880f93
Matthew Cavanagh <maelstrome26@gmail.com>
2016-02-02 20:46:36 +0000

    Reconfigured config module into returning the object only. Changed reference to the object in push_websocket to directly interact with that property

--- config.js
+++ config.js
@@ -23,37 +23,25 @@ let config = {
     },
     debug : {
 
-    }
-};
-
-let supplementalConfig = {
-    worlds : {
-        1: 'Connery',
-        10: 'Miller',
-        13: 'Cobalt',
-        17: 'Emerald',
-        19: 'Jaeger',
-        25: 'Briggs',
-        1000: 'Genudine (PS4US)',
-        1001: 'Palos (PS4US)',
-        1002: 'Crux (PS4US)',
-        1003: 'Searhus (PS4US)',
-        1004: 'Xelas (PS4US)',
-        2000: 'Ceres (PS4EU)',
-        2001: 'Lithcorp (PS4EU)',
-        2002: 'Rashnu (PS4EU)'
-    }
-};
-
-
-module.exports = {
-    "getConfig": () => {
-        return config;
     },
-    "getSupplementalConfig": () => {
-        return supplementalConfig;
-    },
-    "getConfigItem": (item) => {
-        return config[item];
+    supplementalConfig: {
+        worlds : {
+            1: 'Connery',
+            10: 'Miller',
+            13: 'Cobalt',
+            17: 'Emerald',
+            19: 'Jaeger',
+            25: 'Briggs',
+            1000: 'Genudine (PS4US)',
+            1001: 'Palos (PS4US)',
+            1002: 'Crux (PS4US)',
+            1003: 'Searhus (PS4US)',
+            1004: 'Xelas (PS4US)',
+            2000: 'Ceres (PS4EU)',
+            2001: 'Lithcorp (PS4EU)',
+            2002: 'Rashnu (PS4EU)'
+        }
     }
 };
+
+module.exports = config;
