6fb3abf4ecc923acfefe6590a248574a8dbdc602
Matthew Cavanagh <maelstrome26@gmail.com>
2016-07-02 00:33:52 +0100

    ...

--- config.js
+++ config.js
@@ -17,28 +17,26 @@
             census: true
         },
         supplementalConfig: {
-        worlds : {
-            1: 'Connery',
-            10: 'Miller',
-            13: 'Cobalt',
-            17: 'Emerald',
-            19: 'Jaeger',
-            25: 'Briggs',
-            1000: 'Genudine (PS4US)',
-            1001: 'Palos (PS4US)',
-            1002: 'Crux (PS4US)',
-            1003: 'Searhus (PS4US)',
-            1004: 'Xelas (PS4US)',
-            2000: 'Ceres (PS4EU)',
-            2001: 'Lithcorp (PS4EU)',
-            2002: 'Rashnu (PS4EU)'
+            worlds : {
+                1: 'Connery',
+                10: 'Miller',
+                13: 'Cobalt',
+                17: 'Emerald',
+                19: 'Jaeger',
+                25: 'Briggs',
+                1000: 'Genudine (PS4US)',
+                1001: 'Palos (PS4US)',
+                1002: 'Crux (PS4US)',
+                1003: 'Searhus (PS4US)',
+                1004: 'Xelas (PS4US)',
+                2000: 'Ceres (PS4EU)',
+                2001: 'Lithcorp (PS4EU)',
+                2002: 'Rashnu (PS4EU)'
+            },
+            pcWorlds: [1,10,13,17,25],
+            ps4usWorlds: [1000, 1001],
+            ps4euWorlds: [2000, 2001],
         },
-        pcWorlds: [1,10,13,17,25],
-        ps4usWorlds: [1000, 1001],
-        ps4euWorlds: [2000, 2001],
-        },
-        // All events we're going to moni
-        events: [
             'Death',
             'GainExperience',
             'MetagameEvent',
